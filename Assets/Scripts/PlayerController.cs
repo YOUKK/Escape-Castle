f9123f83cd15209dc1607c3f208757b73b2c274d
KimYouKyeong <kimyoukeng@naver.com>
2021-08-14 00:03:23 +0900

    modify PlayerController script(make player run)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 스피드 조정 변수
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    private float currentSpeed;
+
+    // 필요한 컴포넌트
+    private Rigidbody2D myRigid;
+
     void Start()
     {
-        
-    }
+        myRigid = GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
+        // 초기화
+        currentSpeed = walkSpeed;
+    }
     void Update()
     {
-        
+        Move();
+    }
+
+    // 달리기 시도
+    private void TryRun()
+	{
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+            Running();
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+            RunningCancel();
+		}
+	}
+
+    // 달리기
+    private void Running()
+	{
+        currentSpeed = runSpeed;
+	}
+
+    // 달리기 취소
+    private void RunningCancel()
+	{
+        currentSpeed = walkSpeed;
+	}
+
+    // 움직임 실행, 기본 상태는 walk
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveHorizontal = transform.right * horizontalInput;
+        Vector3 moveVertical = transform.up * verticalInput;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * currentSpeed;
+
+        myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
+        // transform.Translate(velocity * Time.deltaTime); 만약 Rigidbody2D 안 쓰면 이걸로 움직일 수 있음
     }
 }
